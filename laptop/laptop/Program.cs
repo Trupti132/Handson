06002dfff8e3143d65d08b971c0e0365d43e4f7c
Trupti132 <gaikwadtrupti2000@gmail.com>
2021-12-30 11:26:05 +0530

    Uploaded

---
+++ laptop/laptop/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laptop
+{
+  
+            class Program
+        {
+            static void Main(string[] args)
+            {
+                Laptop lp = new Laptop();
+
+                lp.ShowDetails(Laptop.Lenove, Laptop.Price1000, Laptop.i3, Laptop.Ram2GB, Laptop.HD500GB);
+                Console.WriteLine("\n");
+
+
+
+                Console.ReadKey();
+            }
+        }
+        class Laptop : LaptopBase
+        {
+            public void ShowDetails(string name, string price, string processor, string ram, string hdd)
+            {
+                Console.WriteLine("Name : " + name);
+                Console.WriteLine("Price : " + price);
+                Console.WriteLine("Processor : " + processor);
+                Console.WriteLine("Ram : " + ram);
+                Console.WriteLine("HDD : " + hdd);
+            }
+        }
+
+        class LaptopBase
+        {
+            public const string Lenove = "Lenovo";
+
+
+            public const string Price1000 = "$1000";
+
+
+            public const string i3 = "i3";
+
+
+            public const string Ram2GB = "2GB";
+
+
+            public const string HD500GB = "500GB";
+
+        }
+    
+}
+    
