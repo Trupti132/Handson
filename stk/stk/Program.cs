0cdf3d55df91eeb87fe9ae0d927fe9063123370c
Trupti132 <gaikwadtrupti2000@gmail.com>
2021-12-30 16:19:35 +0530

    uploaded

---
+++ stk/stk/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace stk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack stack = new Stack();
+            
+
+            for (int i =0; i <3; i++)
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+
+                stack.Push(new Person()
+                {
+                    Name = name,
+                    Age = age
+                });
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Person p = (Person)stack.Pop();
+                Console.WriteLine(p.ToString());
+            }
+            Console.Read();
+        }
+    }
+}
+
+
+
+
+
+
+        
